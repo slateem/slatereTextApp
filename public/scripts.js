6dc2791d282952137811399aa23b0d0cfda62cb2
Eric Slater <slatere@flip3.engr.oregonstate.edu>
2021-05-31 09:00:01 -0700

    word count update

--- public/scripts.js
+++ public/scripts.js
@@ -1,37 +1,41 @@
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//GET ROUTE FOR SAMPLE TEXT: BELOW
+//Function to send word to scraper API and return content
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-var wordsUsed = [];//used to output words user used
 
-//I WILL SEND A WORD TO WILLS API AND RETURN A PARAGRAPH
+var wordsUsed = [];//used to output words user used
+ 
 document.getElementById("sampleContent_submit").addEventListener('click', function(event){
 
-    var req = new XMLHttpRequest();
-    
     //get the word to be sent to the scraping api
     var sampleWord = document.getElementById('sampleContent_input').value
 
     //push word used to wordsUsed array
-    wordsUsed.push(sampleWord);
-
-    //Group member will tell me where to send the word to access the scraper
-    //var webLink = "address/scraper?word=" + sampleWord;    
+    wordsUsed.push(sampleWord);   
   
-    //this is a test weblink
+    //Creates weblink to be sent via a GET request
     var scrapeLink = "https://portfive.net/language-game/scrape?word=" + sampleWord;
 
-    console.log(scrapeLink);
-    //open GET request
-    req.open("GET", scrapeLink, true);
-    console.log("translate text sent")
+    //function to send get request and handle response
+    sendWordGetContent(scrapeLink);
+
+})
+
+
+//function for the GET request and response
+function sendWordGetContent(linkToSend){
+
+    var req = new XMLHttpRequest();
+
+    req.open("GET", linkToSend, true);
+    console.log("translate text sent");
+    console.log(linkToSend);
 
     //create an asynchronous call by adding a listener on the request's load event.
     req.addEventListener('load', function() {
         if(req.status >= 200 && req.status < 400) {
             console.log(req.response)
-            //var response = JSON.parse(req.responseText);
-            //console.log(response);
+           
             //get variables for output
             var scrapedContent = req.response;
             document.getElementById('sampleContent_Output').textContent = scrapedContent;
@@ -42,9 +46,9 @@ document.getElementById("sampleContent_submit").addEventListener('click', functi
         }});
 
     req.send(null);//this is a get request so you are sending null.   
-    event.preventDefault();//
-})
+    event.preventDefault();
 
+}
 
 
 
@@ -90,25 +94,46 @@ document.getElementById("translation_submit").addEventListener('click', function
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Word Counter: Sums up the count of each word in content it is passed. 
 //Sources referenced: https://stackoverflow.com/questions/30906807/word-frequency-in-javascript
+//Sources reference for replacing words: https://stackoverflow.com/questions/9792927/javascript-array-search-and-remove-string
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 document.getElementById("analyze_submit").addEventListener('click', function(event) {
 
     var result = {}; //words and word counts will be added here
-    var get_words = document.getElementById("analyze_input").value; //get the string of words to count
-    console.log(get_words);//console log for testing
+    var getWords = document.getElementById("analyze_input").value; //get the string of words to count
+    console.log(getWords);//console log for testing
+
+    getWords= getWords.toLowerCase();
 
     //replace the periods, commas, and parenthasis, etc...and then split the string on whitespace. Words will be a list of words.
-    var words = get_words.replace(/[.]/g, '').replace(/[;]/g, '').replace(/[#]/g, '').replace(/[0-9]/g, '').replace(/[&]/g, '').replace(/[,]/g, '').replace(/[(]/g, '').replace(/[)]/g, '').replace(/[:]/g, '').replace(/[']/g, '').split(/\s+/);
+    var words = getWords.replace(/[.;#0123456789&,():']/g, '').split(/\s+/);
+
+    //function to delete unimportant words 
+    words = words.filter(wordsToKeep => wordsToKeep != 'a');
+    words = words.filter(wordsToKeep => wordsToKeep != 'an');
+    words = words.filter(wordsToKeep => wordsToKeep != 'the');
+    words = words.filter(wordsToKeep => wordsToKeep != 'is');
+    words = words.filter(wordsToKeep => wordsToKeep != 'this');
+    words = words.filter(wordsToKeep => wordsToKeep != 'and');
+    words = words.filter(wordsToKeep => wordsToKeep != 'as');
+    words = words.filter(wordsToKeep => wordsToKeep != 'of');
+    words = words.filter(wordsToKeep => wordsToKeep != 'than');
+    words = words.filter(wordsToKeep => wordsToKeep != 'then');
+    words = words.filter(wordsToKeep => wordsToKeep != 'to');
+    words = words.filter(wordsToKeep => wordsToKeep != 'by');
+    words = words.filter(wordsToKeep => wordsToKeep != 'in');
+    words = words.filter(wordsToKeep => wordsToKeep != 'what');
+
+    console.log(typeof(words));
     console.log(words)
 
-    var wordCount = {};//object for word counts
+    var wordCount = {};
 
     //for each word, see if it exists in wordCount, if it does, increment it, if it does not then add it to the object
-    words.forEach(function(x) {
-        if (!wordCount[x]) {
-            wordCount[x] = 0;
+    words.forEach(function(wordInList) {
+        if (!wordCount[wordInList]) {
+            wordCount[wordInList] = 0;
         }
-        wordCount[x] += 1;
+        wordCount[wordInList] += 1;
     });
     event.preventDefault();
     
@@ -130,9 +155,10 @@ document.getElementById("analyze_submit").addEventListener('click', function(eve
         var tr = document.createElement('tr');//creates new row
         var td1 = document.createElement('td');//creates new cell
         var td2 = document.createElement('td');//creates new cell
-        tr.style.border = "thin solid #000000";//adds boarder
-        td1.style.border = "thin solid #000000";//adds boarder
-        td2.style.border = "thin solid #000000";//adds boarder
+        
+        var borderList = [tr, td1, td2];
+        
+        addBorder(borderList);
 
         var text1 = document.createTextNode(word);//adds word to first cell
         var text2 = document.createTextNode(wordCount[word]);//adds word count to second cell
@@ -150,3 +176,8 @@ document.body.appendChild(table).setAttribute("id", "tableID");;
    
 })
 
+function addBorder(listToAddBorders){
+
+    for(var x=0; x<listToAddBorders.length; x++)
+        listToAddBorders[x].style.border = "thin solid #000000";
+}
